d634a7ea07a528e58206364ff4c9150575562567
Robert <bintescurobert@yahoo.com>
2019-04-12 09:18:17 +0300

    Argument nume

--- hello.c
+++ hello.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main()
-{ printf("Salut lume");
+ { char v[20];
+  printf("Introduceti numele \n");
+scanf("%s",v);
+ printf("Salut %s",v);
 return 0;
 }
